a826e2660b192f669ee7a45dfa0411ca90ec4ae6
Eric Fitzsimons <ericfitzsimons451@gmail.com>
2019-12-11 15:03:19 -0700

    add styling transitions to projects

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -4,12 +4,30 @@ import palette from '../../Assets/Palette.png'
 import movieTracker from '../../Assets/MovieTracker.png'
 import getSwole from '../../Assets/getSwole.png'
 
-export const Projects = () => {
+const Projects = () => {
   return (
     <div className="projects">
-        <img src={palette} className="palette" alt="Palette Picker" ></img>
-        <img src={movieTracker} className="movieTracker" alt="Palette Picker" ></img>
-        <img src={getSwole} className="getSwole" alt="Palette Picker" ></img>
+      <div className="project-container">
+        <a href="https://github.com/ericfitzsimons451/palette-picker-ui">
+          <h3 className="project-title">Palette Picker</h3>
+          <img src={palette} className="palette" alt="Palette Picker" ></img>
+          <p className="project-description">Choose and save randomly generated color palettes</p>
+        </a>
+      </div>
+      <div className="project-container">
+        <a href="https://github.com/ericfitzsimons451/movie-tracker-trs-ef">
+          <h3 className="project-title">Movie Tracker</h3>
+          <img src={movieTracker} className="movieTracker" alt="Palette Picker" ></img>
+          <p className="project-description">Login to track your favorite movies!</p>
+        </a>
+      </div>
+      <div className="project-container">
+        <a href="https://github.com/ericfitzsimons451/JA-DC-EF-TA--GetSwole">
+          <h3 className="project-title">Get Swole</h3>
+          <img src={getSwole} className="getSwole" alt="Palette Picker" ></img>
+          <p className="project-description">Native application for making and scheduling workouts</p>
+        </a>
+      </div>
     </div>
   )
 }
