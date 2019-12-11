81c6274b28d77409bd2268e26a25c6a3a483af05
Eric Fitzsimons <ericfitzsimons451@gmail.com>
2019-12-10 22:50:09 -0700

    add photos for projects and begin styling

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import './Projects.scss'
 import palette from '../../Assets/Palette.png'
+import movieTracker from '../../Assets/MovieTracker.png'
+import getSwole from '../../Assets/getSwole.png'
 
 export const Projects = () => {
   return (
     <div className="projects">
-      <div className="palette-container">
         <img src={palette} className="palette" alt="Palette Picker" ></img>
-      </div>
-
+        <img src={movieTracker} className="movieTracker" alt="Palette Picker" ></img>
+        <img src={getSwole} className="getSwole" alt="Palette Picker" ></img>
     </div>
   )
 }
