d860d8fbff4e77b1e7584c6bb7ba3179a6f8549b
Eric Fitzsimons <ericfitzsimons451@gmail.com>
2020-05-15 14:56:32 -0600

    trying again

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -39,45 +39,6 @@ const Projects = () => {
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
         </div>
       </div>
-
-
-
-
-      {/* <div className="project-container">
-        <div>
-          <div className="icon-container">
-            <a href="https://www.reactjs.org"><img src={react} className="react" alt="React" /></a>
-            <a href="https://www.redux.js.org"><img src={redux} className="redux" alt="Redux" /></a>
-            <a href="https://www.jestjs.io"><img src={jest} className="jest" alt="Jest" /></a>
-            <a href="https://www.nodejs.org"><img src={nodeJS} className="nodeJS" alt="NodeJS" /></a>
-          </div>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-        </div>
-        <div className="flex-projects">
-          <h3 className="project-title">Movie Tracker</h3>
-          <a href="https://github.com/ericfitzsimons451/movie-tracker-trs-ef" className="center">
-            <img src={movieTracker} className="movieTracker" alt="Movie Tracker" />
-          </a>
-        </div>         
-      </div>
-      <div className="project-container">
-        <div className="flex-projects">
-          <h3 className="project-title">Get Swole</h3>
-          <a href="https://github.com/ericfitzsimons451/JA-DC-EF-TA--GetSwole" className="center">
-            <img src={getSwole} className="getSwole" alt="Get Swole" />
-          </a>
-        </div> 
-        <div>
-          <div className="icon-container">
-            <img src={react} className="react" alt="React" />
-            <img src={sass} className="sass" alt="Sass" />
-            <img src={redux} className="redux" alt="Redux" />
-            <img src={react} className="react" alt="react" />
-          </div>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-        </div>
-      </div>  */}
-
     </div>
   )
 }
